58fa033a797d686469eae95d49e6fd7d21d58443
wwwchendev <wwwchendev@gmail.com>
2024-01-17 00:31:18 +0800

    init🥚: first-commit

---
+++ js/all.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    $('.hambuger-menu').click(function (e) { 
+        e.preventDefault();
+        $('.header ul').toggleClass('active');
+    });
+});
+
+
+$(document).ready(function () {
+    $('#product1 .fa-heart').click(function (e) { 
+        e.preventDefault();
+        $('#product1 .fa-heart').toggleClass('fa-solid');
+    });
+});
+
+$(document).ready(function () {
+    $('#product2 .fa-heart').click(function (e) { 
+        e.preventDefault();
+        $('#product2 .fa-heart').toggleClass('fa-solid');
+    });
+});
+
+$(document).ready(function () {
+    $('#product3 .fa-heart').click(function (e) { 
+        e.preventDefault();
+        $('#product3 .fa-heart').toggleClass('fa-solid');
+    });
+});
+
+$(document).ready(function () {
+    $('#product4 .fa-heart').click(function (e) { 
+        e.preventDefault();
+        $('#product4 .fa-heart').toggleClass('fa-solid');
+    });
+});
